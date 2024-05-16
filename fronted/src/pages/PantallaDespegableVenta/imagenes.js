7b118752b5e5e24267308598cd1662e942ef7f9d
sebas <22sevago@gmail.com>
2024-05-16 12:40:53 -0500

    AEA

--- fronted/src/pages/PantallaDespegableVenta/imagenes.js
+++ fronted/src/pages/PantallaDespegableVenta/imagenes.js
@@ -4,6 +4,7 @@ import Ijc5xangrw540x1 from "./img/ljc1xangrw-540x-1.png";
 import Ijc5xangrw540x11 from "./img/ljc1xangrw-540x-1-1.png";
 import trash21 from "./img/trash-2-1.svg";
 import xSquare from "./img/x-square.svg";
+import vector from "./img/vector.svg";
 
 export default {
     "line13": line13,
@@ -11,5 +12,6 @@ export default {
     "Ijc5xangrw540x1": Ijc5xangrw540x1,
     "Ijc5xangrw540x11": Ijc5xangrw540x11,
     "trash21": trash21,
-    "xSquare": xSquare
+    "xSquare": xSquare,
+    "vector":vector
 };
