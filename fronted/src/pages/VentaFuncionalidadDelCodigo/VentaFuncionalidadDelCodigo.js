1f13acaa03d307a74c55d81ca5b38fe943fd311b
sebas <22sevago@gmail.com>
2024-05-16 11:39:27 -0500

    Funcionalidad a page FuncionalidadDelCodigo

--- fronted/src/pages/VentaFuncionalidadDelCodigo/VentaFuncionalidadDelCodigo.js
+++ fronted/src/pages/VentaFuncionalidadDelCodigo/VentaFuncionalidadDelCodigo.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './globals.css';
 import './style.css';
+import imagenes from './imagenes'; // Importa el archivo imagenes.js
 
 const VentaFuncionalidadDelCodigo = () => {
   return (
@@ -19,32 +20,32 @@ const VentaFuncionalidadDelCodigo = () => {
               </p>
             </div>
           </div>
-          <img class="frame" src="img/frame.svg" />
-          <div class="div-wrapper"><div class="text-wrapper-2">Volver</div></div>
+          <img class="frame" src={imagenes.frame} alt="Frame" />
+          <Link to="/venta-codigo" class="div-wrapper"><div class="text-wrapper-2">Volver</div></Link>
         </div>
         <div class="frame-2">
           <div class="group">
             <div class="text-wrapper-3">GAME CYCLE</div>
-            <img class="vector" src="img/vector.svg" />
+            <img class="vector" src={imagenes.vector} alt="Vector" />
           </div>
           <div class="frame-3">
             <div class="text-wrapper-4">Buscar producto</div>
             <div class="overlap-group">
               <div class="text-wrapper-5">Todas las categorías</div>
-              <img class="chevron-down" src="img/chevron-down.svg" />
+              <img class="chevron-down" src={imagenes.chevronDown} alt="Chevron Down" />
             </div>
-            <img class="line" src="img/line-7.svg" />
-            <img class="img" src="img/frame-198.svg" />
+            <img class="line" src={imagenes.line7} alt="Line" />
+            <img class="img" src={imagenes.frame198} alt="Frame 198" />
           </div>
           <div class="frame-wrapper">
             <div class="frame-4">
               <div class="frame-5">
                 <div class="frame-6">
-                  <img class="group-2" src="img/group.png" />
+                  <img class="group-2" src={imagenes.group} alt="Group" />
                   <div class="frame-7"><div class="text-wrapper-6">Delivery</div></div>
                 </div>
                 <div class="frame-8">
-                  <img class="group-3" src="img/group-95.png" />
+                  <img class="group-3" src={imagenes.group95} alt="Group 95" />
                   <div class="frame-9"><div class="text-wrapper-7">Retornar</div></div>
                 </div>
               </div>
@@ -53,25 +54,25 @@ const VentaFuncionalidadDelCodigo = () => {
           <div class="frame-10">
             <div class="ll-menos-wrapper"><div class="ll-menos">Llámenos</div></div>
             <div class="frame-11">
-              <img class="headphones-svgrepo" src="img/headphones-svgrepo-com-1.svg" />
+              <img class="headphones-svgrepo" src={imagenes.headphonesSvgrepoComL} alt="Headphones SVG" />
               <div class="text-wrapper-8">+51 964542293</div>
             </div>
           </div>
           <div class="frame-12">
-            <img class="user" src="img/user.svg" />
+            <img class="user" src={imagenes.user} alt="User" />
             <div class="frame-13">
               <div class="frame-14">
                 <div class="group-wrapper">
                   <div class="overlap-group-wrapper">
                     <div class="overlap-group-2">
                       <div class="text-wrapper-9">3</div>
-                      <img class="shopping-cart" src="img/shopping-cart.svg" />
+                      <img class="shopping-cart" src={imagenes.shoppingCart} alt="Shopping Cart" />
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <img class="vector-2" src="img/vector-1.svg" />
+            <img class="vector-2" src={imagenes.vector1} alt="Vector 1" />
           </div>
         </div>
         <div class="frame-15">
@@ -84,12 +85,12 @@ const VentaFuncionalidadDelCodigo = () => {
               </div>
               <div class="text-wrapper-10">Búsqueda por categoría</div>
             </div>
-            <div class="text-wrapper-11">Inicio</div>
-            <div class="text-wrapper-12">Nuevo</div>
-            <div class="text-wrapper-13">Usado</div>
-            <div class="text-wrapper-14">Vender</div>
+            <Link to="/inicio" class="text-wrapper-11">Inicio</Link>
+            <Link to="/venta-nuevos" class="text-wrapper-12">Nuevo</Link>
+            <Link to="/venta-usados" class="text-wrapper-13">Usado</Link>
+            <Link to="/venta-codigo" class="text-wrapper-14">Vender</Link>
           </div>
-          <img class="frame-17" src="img/frame-118.svg" />
+          <img class="frame-17" src={imagenes.frame118} alt="Frame 118" />
         </div>
       </div>
     </div>
@@ -97,3 +98,4 @@ const VentaFuncionalidadDelCodigo = () => {
 };
 
 export default VentaFuncionalidadDelCodigo;
+
