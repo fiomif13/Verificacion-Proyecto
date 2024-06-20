a4ec5cfaeec105d85c98687013632bd6275b1c85
sebas <22sevago@gmail.com>
2024-06-19 23:38:37 -0500

    ctualizar base de datos :wq

--- ux.sql
+++ ux.sql
@@ -16,6 +16,62 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorias` (
+  `categoria_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(200) NOT NULL,
+  PRIMARY KEY (`categoria_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,'accion'),(2,'aventura'),(3,'estrategia'),(4,'deportes'),(5,'rpg'),(6,'simulacion');
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `juegos`
+--
+
+DROP TABLE IF EXISTS `juegos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `juegos` (
+  `juego_id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `descripcion` text NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `fecha_lanzamiento` date NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `fecha_creacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`juego_id`),
+  KEY `categoria_id` (`categoria_id`),
+  CONSTRAINT `juegos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`categoria_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `juegos`
+--
+
+LOCK TABLES `juegos` WRITE;
+/*!40000 ALTER TABLE `juegos` DISABLE KEYS */;
+INSERT INTO `juegos` VALUES (8,'IMAGEN.png-2024-06-20','DESCRIPCION IMAGEN',999.00,'2222-02-12',3,10,'2024-06-20 00:32:46'),(9,'PRUEBA.png','123',111.00,'2001-02-12',2,12,'2024-06-20 00:36:07'),(10,'Captura de pantalla 2024-03-01 113047.png','ASDASD',123.00,'3000-02-12',3,540,'2024-06-20 02:31:36'),(11,'Captura de pantalla 2024-03-01 113047.png','ASDASD',123.00,'3000-02-12',3,540,'2024-06-20 02:31:38'),(12,'Captura de pantalla 2024-03-01 113047.png','ASDASD',123.00,'3000-02-12',3,540,'2024-06-20 02:31:40');
+/*!40000 ALTER TABLE `juegos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `usuarios`
 --
 
@@ -30,7 +86,7 @@ CREATE TABLE `usuarios` (
   `apellido` varchar(25) NOT NULL,
   `fecha_creacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`usuario_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +95,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'admin','admin','admin@gmail.com','admin','2024-06-16 05:27:37');
+INSERT INTO `usuarios` VALUES (1,'admin','admin','admin@gmail.com','admin','2024-06-16 05:27:37'),(2,'Sebastián','Sebastian','22sevago@gmail.com','Vásquez ','2024-06-17 22:54:41'),(3,'','Sebastián','Sebastián','','2024-06-19 01:48:16'),(4,'asdasd','Sebastián','Sebastián','asd','2024-06-19 03:05:17'),(5,'','Sebastián','Sebastián','','2024-06-19 03:54:15'),(6,'','Sebastián','Sebastián','','2024-06-19 19:17:17');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-17 14:14:00
+-- Dump completed on 2024-06-19 23:35:24
