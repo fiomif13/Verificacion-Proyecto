ba3cad1d8be6623d48c0627a41764fc89935da5e
sebas <22sevago@gmail.com>
2024-06-21 11:14:16 -0500

    Descripción del juego dinámica en la página JuegosVenta

--- fronted/src/pages/DetalleDelProducto/DetalleDelProducto.js
+++ fronted/src/pages/DetalleDelProducto/DetalleDelProducto.js
@@ -1,56 +1,89 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import imagenes from './imagenes'; // Importar el archivo imagenes.js
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+import imagenes from './imagenes';
 import './globals.css';
 import './style.css';
 
 const DetalleProducto = () => {
+  const { juego_id } = useParams();
+  const [juego, setJuego] = useState(null);
+  const [categorias, setCategorias] = useState([]);
+  const [estados, setEstados] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchJuego = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/detalle-producto/${juego_id}`);
+        setJuego(response.data);
+      } catch (error) {
+        console.error('Error fetching game details:', error);
+        setError('Error fetching game details');
+      }
+    };
+
+    const fetchCategorias = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/categorias');
+        setCategorias(response.data);
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+      }
+    };
+
+    const fetchEstados = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/estados');
+        setEstados(response.data);
+      } catch (error) {
+        console.error('Error fetching states:', error);
+      }
+    };
+
+    fetchJuego();
+    fetchCategorias();
+    fetchEstados();
+  }, [juego_id]);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!juego) {
+    return <p>Cargando...</p>;
+  }
+
+  // Encuentra el nombre de la categoría y el estado
+  const categoriaNombre = categorias.find(cat => cat.categoria_id === juego.categoria_id)?.nombre;
+  const estadoNombre = estados.find(est => est.estado_id === juego.estado_id)?.nombre;
+
+  console.log(juego);
   return (
     <div className="detalle-del-producto">
       <div className="div">
         <div className="frame">
-          <div className="div-wrapper"><div className="text-wrapper">Acción</div></div>
-          <div className="div-wrapper"><div className="text-wrapper">Aventura</div></div>
-          <div className="div-wrapper"><div className="text-wrapper">Mundo Abierto</div></div>
+          <div className="div-wrapper"><div className="text-wrapper">{categoriaNombre}</div></div>
         </div>
         <Link to="/carrito-compras" className="frame-2"><div className="text-wrapper-2">Añadir al carro</div></Link>
-        <div className="frame-3">
-          <img className="img" src={imagenes.frame} alt="frame" />
-          <div className="text-wrapper-3">1</div>
-          <img className="img" src={imagenes.frame1181} alt="frame-1" />
-        </div>
-        <div className="frame-4"><div className="text-wrapper-4">-9%</div></div>
-        <div className="text-wrapper-5">Marvel Spiderman</div>
+        <div className="text-wrapper-5">{juego.titulo}</div>
         <div className="frame-5">
-          <div className="text-wrapper-6">S/100</div>
-          <div className="text-wrapper-7">S/110</div>
+          <div className="text-wrapper-6">S/{juego.precio}</div>
         </div>
-        <div className="text-wrapper-8">Estado: Nuevo</div>
+        <div className="text-wrapper-8">{estadoNombre}</div>
         <p className="p">
-          Siente todo el poder de un Spider-Man más experimentado con combate improvisado, acrobacias dinámicas,
-          recorrido urbano fluido e interacciones con el entorno.
+          {juego.descripcion}
         </p>
         <div className="text-wrapper-9">Géneros</div>
-        <img className="image" src={imagenes.vector} alt="image-1" />
-        <img className="image-2" src={imagenes.image2} alt="image-2" />
-        <img className="image-3" src={imagenes.image5} alt="image-3" />
-        <img className="image-4" src={imagenes.image6} alt="image-4" />
+        <img src={`http://localhost:3001/uploads/${juego.titulo}`} alt={juego.titulo} />
         <div className="frame-6">
           <div className="frame-7">
-            <img className="star" src={imagenes.starL} alt="star-1" />
-            <img className="star" src={imagenes.star2} alt="star-2" />
-            <img className="star" src={imagenes.star3} alt="star-3" />
-            <img className="star" src={imagenes.star4} alt="star-4" />
-            <img className="star-2" src={imagenes.star5} alt="star-5" />
           </div>
-          <p className="element"><span className="span">4.5/</span> <span className="text-wrapper-10">5</span></p>
         </div>
-        <img className="line" src={imagenes.line3} alt="line-3" />
-        <img className="line-2" src={imagenes.line7} alt="line-3" />
-        <img className="line-3" src={imagenes.line7} alt="line-3" />
         
       </div>
     </div>
+    
   );
 };
 
