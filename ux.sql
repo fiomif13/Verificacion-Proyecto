49fb1efd91ed6420762fdf716b7091dcdef0f1d6
sebas <22sevago@gmail.com>
2024-06-28 15:39:36 -0500

    Actualización de la base de datos

--- ux.sql
+++ ux.sql
@@ -40,6 +40,36 @@ INSERT INTO `categorias` VALUES (1,'accion'),(2,'aventura'),(3,'estrategia'),(4,
 UNLOCK TABLES;
 
 --
+-- Table structure for table `detalle`
+--
+
+DROP TABLE IF EXISTS `detalle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detalle` (
+  `detalle_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pedido_id` int(11) DEFAULT NULL,
+  `juego_id` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  PRIMARY KEY (`detalle_id`),
+  KEY `pedido_id` (`pedido_id`),
+  KEY `juego_id` (`juego_id`),
+  CONSTRAINT `detalle_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`pedido_id`),
+  CONSTRAINT `detalle_ibfk_2` FOREIGN KEY (`juego_id`) REFERENCES `juegos` (`juego_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalle`
+--
+
+LOCK TABLES `detalle` WRITE;
+/*!40000 ALTER TABLE `detalle` DISABLE KEYS */;
+INSERT INTO `detalle` VALUES (1,1,6,7),(2,1,2,4),(3,1,11,2),(4,2,2,4);
+/*!40000 ALTER TABLE `detalle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `estado`
 --
 
@@ -85,7 +115,7 @@ CREATE TABLE `juegos` (
   KEY `estado_id` (`estado_id`),
   CONSTRAINT `juegos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`categoria_id`),
   CONSTRAINT `juegos_ibfk_2` FOREIGN KEY (`estado_id`) REFERENCES `estado` (`estado_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,8 +124,35 @@ CREATE TABLE `juegos` (
 
 LOCK TABLES `juegos` WRITE;
 /*!40000 ALTER TABLE `juegos` DISABLE KEYS */;
-INSERT INTO `juegos` VALUES (2,'Juego2.png','asd',12.00,'1212-12-12',1,12,1,'2024-06-20 07:10:02'),(3,'Juego1.png','USADO',12.00,'1221-12-12',4,12,3,'2024-06-20 07:11:34'),(4,'Juego1.png','asd',12.00,'1321-03-12',1,12,3,'2024-06-20 07:30:11'),(5,'Juego1.png','asd',12.00,'1321-03-12',1,12,3,'2024-06-20 07:30:48'),(6,'Juego1.png','ASDASD',12.00,'1212-12-12',4,122,1,'2024-06-20 14:37:13');
+INSERT INTO `juegos` VALUES (2,'Juego2.png','asd',12.00,'1212-12-12',1,12,1,'2024-06-20 07:10:02'),(3,'Juego1.png','USADO',12.00,'1221-12-12',4,12,3,'2024-06-20 07:11:34'),(4,'Juego1.png','asd',12.00,'1321-03-12',1,12,3,'2024-06-20 07:30:11'),(5,'Juego1.png','asd',12.00,'1321-03-12',1,12,3,'2024-06-20 07:30:48'),(6,'Juego1.png','ASDASD',12.00,'1212-12-12',4,122,1,'2024-06-20 14:37:13'),(7,'asd','asd',123.00,'1212-12-12',1,1,1,'2024-06-22 01:01:44'),(8,'asd','asdas',123.00,'3234-02-12',2,1,1,'2024-06-22 01:09:01'),(9,'Captura de pantalla 2024-04-19 165922.png','asd',123.00,'1212-12-12',2,21,1,'2024-06-22 04:51:26'),(10,'Captura de pantalla 2024-04-19 165933.png','asd',12.00,'1212-12-12',1,1,1,'2024-06-22 04:52:31'),(11,'Captura de pantalla 2024-03-01 113047.png','asd',12.00,'1212-12-12',2,1,3,'2024-06-23 19:14:11');
 /*!40000 ALTER TABLE `juegos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido`
+--
+
+DROP TABLE IF EXISTS `pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedido` (
+  `pedido_id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario_id` int(11) DEFAULT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`pedido_id`),
+  KEY `usuario_id` (`usuario_id`),
+  CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido`
+--
+
+LOCK TABLES `pedido` WRITE;
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+INSERT INTO `pedido` VALUES (1,2,'2024-06-28 20:33:33'),(2,2,'2024-06-28 20:34:47');
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -135,4 +192,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-20 14:25:01
+-- Dump completed on 2024-06-28 15:38:29
